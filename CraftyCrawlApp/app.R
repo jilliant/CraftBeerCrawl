47f227db2fac3d51e49cdd7dd50f5304546e8cdb
jilliant <jillian.tallboys@gmail.com>
2018-05-27 14:51:47 +1000

    IT WORKS! YAY!

--- CraftyCrawlApp/app.R
+++ CraftyCrawlApp/app.R
@@ -1,4 +1,4 @@
-# Global
+# Global ----
 library(shiny)
 library(leaflet)
 library(dplyr)
@@ -16,77 +16,41 @@ IconBrew <- makeIcon(iconUrl = "Icons/barrel-3.png",
 IconBar <- makeIcon(iconUrl = "Icons/beer-2.png",
                     iconAnchorX = 18, iconAnchorY = 12)
 
-# TODO Save as rdata and load
 CraftBeer  <- CraftBeer %>% 
   mutate(ID = as.integer(rownames(CraftBeer)))
 
 # Find nearest neighbours
 CraftBeerNN <- data.frame(cbind(CraftBeer$lat, CraftBeer$lon))
 closestCraftBeer <- nn2(data = CraftBeerNN, k=6)[[1]]
-
 closestCraftBeer <- data.frame(closestCraftBeer)
 
+# Convert index to names
 closestCraftBeerDT <- closestCraftBeer %>%
   mutate_all(funs(CraftBeer$Venue[match(., CraftBeer$ID)])) %>%
   rename(Origin = X1, "Stop 2" = X2, "Stop 3" = X3, "Stop 4" = X4, "Stop 5" = X5, "Stop 6" = X6) %>% 
   arrange(Origin)
 closestCraftBeerDT
 
-# add gomap.js
-# https://github.com/rstudio/shiny-examples/tree/master/063-superzip-example
-# 
-#   Sample <- Sample %>% 
-#     inner_join(CraftBeer, by = c( "V1" = "Venue" )) %>% 
-#     select("id","V1", "lat", "lon")
-#   
-#   Sample_tour <- Sample
-#   
-#   # # turn the co-ords into a matrix 
-#   # coords.df <- data.frame(long=Sample$lon, lat=Sample$lat)
-#   # coords.mx <- as.matrix(coords.df)
-#   # 
-#   # # Compute great-circle distance matrix
-#   # dist.mx <- spDists(coords.mx, longlat=TRUE)
-#   # 
-#   # # TSP object
-#   # tsp.ins <- tsp_instance(coords.mx, dist.mx )
-#   # 
-#   # # Solve 
-#   # tour <- run_solver(tsp.ins, method="nn", start = 1)
-#   # 
-#   # # Permutation Vector 
-#   # tour_order <- as.integer(tour)
-#   # 
-#   # # Plot
-#   # autoplot(tsp.ins, tour)
-#   # 
-#   # # reorder
-#   # Sample_tour <- Sample[match(tour_order, Sample$id),]
-#   # 
-#   # On a Map
-#   Sample_tour$lat <- as.double(Sample_tour$lat)
-#   Sample_tour$lon <- as.double(Sample_tour$lon)
-#   })
+# TODO Save as rdata and load
 
-
-  
-
-# A Basic App
+# UI ---- 
 ui <- fluidPage(
+  
   titlePanel("Crafty Crawl"),
+  
   mainPanel(
     leafletOutput("map"),
     br(), br(),
-    #tableOutput("results"),
+    #tableOutput("chk"),
     tableOutput("itinerary")),
+  
   sidebarPanel(
-    ### User chooses the tour to map
     selectInput("venueInput", "Enter a starting point",
                 CraftBeer$Venue,
                 selected = "Boatrocker Brewers & Distillers"),
-    selectInput("stops", "Number of Stops",
-                c(2,3,4,5,6),
-                selected = "5")
+    radioButtons("stops", h3("Number of Venues"),
+                 choices = list("4 Venues" = 4, "5 Venues" = 5,
+                                "6 Venues" = 6),selected = 5)
   ))
 
 server <- function(input, output, session){
@@ -103,15 +67,41 @@ server <- function(input, output, session){
       mutate(id = seq.int(nrow(Locations)))
     Locations
     
-    filtered <- Locations
+    # TSP 
     
-    leaflet(filtered) %>%
+    ## turn the co-ords into a matrix
+    coords.df <- data.frame(long=Locations$lon, lat=Locations$lat)
+    coords.mx <- as.matrix(coords.df)
+
+    ## Compute great-circle distance matrix
+    dist.mx <- spDists(coords.mx, longlat=TRUE)
+    
+    ## Create TSP object
+    tsp.ins <- tsp_instance(coords.mx, dist.mx )
+    
+    ## Solve
+    tour <- run_solver(tsp.ins, method="nn", start = 1)
+    
+    ## Get Permutation Vector
+    tour_order <- as.integer(tour)
+    
+    ## reorder data frame
+    Locations_tour <- Locations[match(tour_order, Locations$id),]
+    
+    ## Cast for Mapping
+    Locations_tour$lat <- as.double(Locations_tour$lat)
+    Locations_tour$lon <- as.double(Locations_tour$lon)
+
+    Itinerary <- Locations_tour
+    
+    leaflet(Itinerary) %>%
       addTiles(urlTemplate = 'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png',
                attribution = attribution1) %>%
       addMarkers(~as.numeric(lon), ~as.numeric(lat),
                  icon = IconCraft,
                  #popup = ~as.character(V1),#add in address or votes or link to untappd?
-                 label = ~as.character(V1))
+                 label = ~as.character(V1)) %>% 
+    addPolylines(lng = ~as.numeric(lon), lat = ~as.numeric(lat))
   })
 
   output$itinerary <- renderTable({
@@ -119,6 +109,19 @@ server <- function(input, output, session){
       filter(Origin == input$venueInput) %>% 
       select(1:input$stops)
     ShowItinerary
+  })
+  
+  output$chk <- renderTable({
+    ShowItinerary <- closestCraftBeerDT %>% 
+      filter(Origin == input$venueInput) %>% 
+      select(1:input$stops)
+    Locations <- as.data.frame(t(ShowItinerary))
+    Locations <- Locations %>%
+      inner_join(CraftBeer, by = c( "V1" = "Venue" )) %>%
+      select("V1", "lat", "lon") %>% 
+      mutate(id = seq.int(nrow(Locations)))
+    Locations
+    
   })
 }
 
