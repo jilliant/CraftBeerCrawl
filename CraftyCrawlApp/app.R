dc4e907e0eddb44e5ddb577a625d44c4be53529b
jilliant <jillian.tallboys@gmail.com>
2018-05-27 17:07:17 +1000

    debugging for deployment

--- CraftyCrawlApp/app.R
+++ CraftyCrawlApp/app.R
@@ -2,6 +2,9 @@
 library(shiny)
 library(leaflet)
 library(dplyr)
+library(sp)
+library(tspmeta)
+library(RANN)
 
 setwd("~/Desktop/SomethingWithNumbers/CraftBeerCrawl")
 load("Data/CraftBeer.rdata")
@@ -39,6 +42,8 @@ titlePanel("Crafty Crawl"),
 
 mainPanel(
   tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+  br(), br(),
+  tableOutput("chk"),
   leafletOutput("map")),
 sidebarPanel(
   tags$style(".well {background-color:#e6e6e6;}"),
@@ -47,10 +52,9 @@ sidebarPanel(
 
     selectInput("venueInput", "Enter a starting point",
                 CraftBeer$Venue,
-                selected = "Boatrocker Brewers & Distillers"),
+                selected = "Stomping Ground Brewing Co"),
     radioButtons("stops", "Number of Venues",
-                 choices = list("4 Venues" = 4, "5 Venues" = 5,
-                                "6 Venues" = 6),selected = 5),
+                 choices = list(4,5,6), selected = 5),
     tableOutput("itineraryT")
   ))
 
@@ -58,15 +62,24 @@ server <- function(input, output, session){
 
   output$map <- renderLeaflet({
 
-    ShowItinerary <- closestCraftBeerDT %>% 
-      filter(Origin == input$venueInput) %>% 
-      select(1:input$stops)
-    Locations <- as.data.frame(t(ShowItinerary))
-    Locations <- Locations %>%
-      inner_join(CraftBeer, by = c( "V1" = "Venue" )) %>%
-      select("V1", "lat", "lon") %>% 
-      mutate(id = seq.int(nrow(Locations)))
-    Locations
+    
+      Itinerary <- closestCraftBeerDT %>% 
+        filter(Origin == input$venueInput) 
+      Itinerary <- Itinerary[, 1:input$stops]
+      Itinerary <- as.data.frame(t(Itinerary))
+      colnames(Itinerary) <- "Venue"
+      Itinerary$Venue <- as.character(Itinerary$Venue)
+      
+      # Itinerary <- closestCraftBeerDT %>% 
+      #   filter(Origin == "Stomping Ground Brewing Co") 
+      # Itinerary <- Itinerary[, 1:5]
+      # Itinerary <- as.data.frame(t(Itinerary))
+      # colnames(Itinerary) <- "Itinerary"
+        
+      Locations <- Itinerary %>%
+        inner_join(CraftBeer, by = c( "Venue" = "Venue" )) %>% 
+        mutate(id = seq.int(nrow(Itinerary)))
+      Locations
     
     # TSP 
     
@@ -103,19 +116,31 @@ server <- function(input, output, session){
       addMarkers(~as.numeric(lon), ~as.numeric(lat),
                  icon = IconCraft,
                  #popup = ~as.character(V1),#add in address or votes or link to untappd?
-                 label = ~as.character(V1)) %>% 
+                 label = ~as.character(Venue)) %>% 
     addPolylines(lng = ~as.numeric(lon), lat = ~as.numeric(lat))
   })
   
 
   output$itineraryT <- renderTable({
     ShowItineraryT <- closestCraftBeerDT %>% 
-      filter(Origin == input$venueInput) %>% 
-      select(1:input$stops)
+      filter(Origin == input$venueInput) 
+    ShowItineraryT <- ShowItineraryT[, 1:input$stops]
     ShowItineraryT <- as.data.frame(t(ShowItineraryT))
     colnames(ShowItineraryT) <- "Itinerary"
     ShowItineraryT
   })
+  
+  # output$chk <- renderTable({
+  #   ShowItinerary <- closestCraftBeerDT %>% 
+  #     filter(Origin == input$venueInput)# %>% 
+  #     #select(1:input$stops)
+  #   Locations <- as.data.frame(t(ShowItinerary))
+  #   Locations <- Locations %>%
+  #     inner_join(CraftBeer, by = c( "V1" = "Venue" )) %>%
+  #     select("V1", "lat", "lon") %>% 
+  #     mutate(id = seq.int(nrow(Locations)))
+  #   Locations
+# })
 }
 
 shinyApp(ui, server)
