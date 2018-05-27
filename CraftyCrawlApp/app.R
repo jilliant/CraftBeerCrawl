af47aef27f550116b0a46085b1e4b3d195cc929c
jilliant <jillian.tallboys@gmail.com>
2018-05-27 12:57:02 +1000

    UI started

--- CraftyCrawlApp/app.R
+++ CraftyCrawlApp/app.R
@@ -1,48 +1,90 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# Global
 library(shiny)
+library(leaflet)
+library(dplyr)
+
+setwd("~/Desktop/SomethingWithNumbers/CraftBeerCrawl")
+load("Data/CraftBeer.rdata")
+
+# Map things
+attribution1 <- 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | Icons made by <a href="http://www.flaticon.com/authors/nas-ztudio" title="Nas Ztudio">Nas Ztudio</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC BY 3.0 </a>'  
+
+IconCraft <- makeIcon(iconUrl = "Icons/beer-1.png",
+                      iconAnchorX = 18, iconAnchorY = 12)
+IconBrew <- makeIcon(iconUrl = "Icons/barrel-3.png",
+                     iconAnchorX = 18, iconAnchorY = 12)
+IconBar <- makeIcon(iconUrl = "Icons/beer-2.png",
+                    iconAnchorX = 18, iconAnchorY = 12)
+
+# TODO Save as rdata and load
+CraftBeer  <- CraftBeer %>% 
+  mutate(ID = as.integer(rownames(CraftBeer)))
+
+# Find nearest neighbours
+CraftBeerNN <- data.frame(cbind(CraftBeer$lat, CraftBeer$lon))
+closestCraftBeer <- nn2(data = CraftBeerNN, k=6)[[1]]
+
+closestCraftBeer <- data.frame(closestCraftBeer)
+
+closestCraftBeerDT <- closestCraftBeer %>%
+  mutate_all(funs(CraftBeer$Venue[match(., CraftBeer$ID)])) %>%
+  rename(Origin = X1, "Stop 2" = X2, "Stop 3" = X3, "Stop 4" = X4, "Stop 5" = X5, "Stop 6" = X6) %>% 
+  arrange(Origin)
+closestCraftBeerDT
+
+# add gomap.js
+# https://github.com/rstudio/shiny-examples/tree/master/063-superzip-example
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+#ui <- fluidPage(
+ui <- bootstrapPage(
+    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+    leafletOutput("map", width = "100%", height = "100%"),
+    absolutePanel(top = 10, right = 10,
+                  selectizeInput("Origin",
+                                 label = ("Origin"),
+                                 multiple = FALSE,
+                                 choices = closestCraftBeerDT$Origin,
+                                 options = list(
+                                   placeholder = 'Select starting venue, type to search',
+                                   onInitialize = I('function() { this.setValue(""); }')
+                                 )),
+                  
+                  selectInput("stops", "Number of Stops",
+                              c(2,3,4,5,6)
+                  ),
+                  submitButton("Go!")
+    )
+  )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  
+  
+  
+  
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  output$map <- renderLeaflet({
+    leaflet()%>%
+      setView(133.8807, -27.6980,4) %>% 
+      addTiles(urlTemplate = 'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', 
+               attribution = attribution1
+      ) %>%
+      addMarkers(lng = Sample_tour$lon, lat = Sample_tour$lat,
+                 icon = IconCraft,
+                 popup = paste(Sample_tour$V1),
+                 group = "Location"
+      ) %>% 
+      addPolylines(lng = Sample_tour$lon, lat = Sample_tour$lat)
+  })
+  
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  # Data Table
+
+  
+
 }
 
 # Run the application 
